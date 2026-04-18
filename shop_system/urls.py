e927a8b74061ef036d41b45a39260b69ce21b15a
Abisheak2783 <abisheak2783@users.noreply.github.com>
2026-04-18 11:43:48 +0530

    Fix missing import in urls.py

--- shop_system/urls.py
+++ shop_system/urls.py
@@ -8,6 +8,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 from django.views.static import serve
 from django.urls import re_path
